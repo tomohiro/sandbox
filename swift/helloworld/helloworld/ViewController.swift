7bb7b5bb4ba689ff47ed639489dee4a7f7d819d2
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-10-23 23:56:12 +0900

    Hello, World by Swift 2

---
+++ swift/helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Tomohiro TAIRA on 10/23/15.
+//  Copyright © 2015 Tomohiro TAIRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("Hell World!")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
