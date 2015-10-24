03abfa603e911c352349b8d35c15ac71bb8aa3ca
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-10-24 21:34:59 +0900

    Add label and buttons

--- swift/helloworld/helloworld/ViewController.swift
+++ swift/helloworld/helloworld/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UILabel!
+
     override func viewDidLoad() {
-        print("Hell World!")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func messageShow(sender: AnyObject) {
+        message.hidden = false
+    }
+
+    @IBAction func messageHide(sender: AnyObject) {
+        message.hidden = true
     }
 
 }
