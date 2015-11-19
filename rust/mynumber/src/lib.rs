b4fed941c59b198fc84e266092f4e7691e1fa720
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-11-19 15:24:11 +0900

    Support Result type

--- rust/mynumber/src/lib.rs
+++ rust/mynumber/src/lib.rs
@@ -1,7 +1,15 @@
-pub fn validate(mynumber: &str) -> bool {
-  let numbers = mynumber.as_bytes();
-  if numbers.len() != 12 {
-    return false;
-  }
-  return true;
+pub enum ValidateError { InvalidMyNumberLength, InvalidType }
+
+pub fn validate(mynumber: &str) -> Result<(), ValidateError> {
+    if mynumber.len() != 12 {
+        return Err(ValidateError::InvalidMyNumberLength);
+    }
+
+    for i in mynumber.chars() {
+        if !i.is_numeric() {
+            return Err(ValidateError::InvalidType)
+        }
+    }
+
+    return Ok(());
 }
