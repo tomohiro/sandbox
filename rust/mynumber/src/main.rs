8285bffb4b8aa10d639efe0063a06263d0da3399
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-12-07 18:16:41 +0900

    Format by `rustfmt`

--- rust/mynumber/src/main.rs
+++ rust/mynumber/src/main.rs
@@ -5,7 +5,7 @@ use mynumber::individual;
 fn main() {
     let number = "123456789018";
     match individual::verify(number) {
-      Ok(()) => println!("{} is valid", number),
-      Err(e) => println!("{} is invalid: {:?}", number, e),
+        Ok(()) => println!("{} is valid", number),
+        Err(e) => println!("{} is invalid: {:?}", number, e),
     }
 }
